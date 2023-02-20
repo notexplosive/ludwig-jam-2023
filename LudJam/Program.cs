26e9ccff630c83beed8bb1a54abedc08ea1d6985
notexplosive <thenotexplosive@gmail.com>
2023-02-19 19:25:14 -0800

    (automated) Initial Commit

---
+++ LudJam/Program.cs
@@ -0,0 +1,11 @@
+﻿using ExplogineDesktop;
+using ExplogineMonoGame;
+using ExplogineMonoGame.Cartridges;
+using Microsoft.Xna.Framework;
+
+var config = new WindowConfigWritable
+{
+    WindowSize = new Point(1600, 900),
+    Title = "NotExplosive.net"
+};
+Bootstrap.Run(args, new WindowConfig(config), (runtime) => new BlankCartridge(runtime));
---
+++ rebuild_content.bat
@@ -0,0 +1,1 @@
+dotnet run --project .\explogine\Tools\AssetBuilder\AssetBuilder.csproj -- .\Assets\Content\Content.mgcb
